eddad10d85351a7db37eb0398df32ad5b4991ccb
Dany Herrera <A00838702@tec.mx>
2026-01-17 17:39:36 -0600

    feat: model training and metrics logging

--- demo.py
+++ demo.py
@@ -52,8 +52,27 @@ if __name__ == "__main__":
     train_x = train.drop(["quality"], axis=1) #independent variables, drop dependant
     train_y = train[["quality"]] #desired prediction values
     test_x = test.drop(["quality"], axis=1)
-    test_x = test[["quality"]]
+    test_y = test[["quality"]]
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5 #learning rate
     l1_ratio = float(sys.argv[2]) if sys.argv > 1 else 0.5
     
+
+    with mlflow.start_run(): #automatic experiment tool
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predictions = lr.predict(test_x)
+
+        rmse, mae, r2 = eval_metrics(test_y, predictions)
+
+        #log used parameters and metric values
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+
+        
+
+
